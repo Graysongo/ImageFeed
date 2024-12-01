6e0afaa93a286e2cb439f18a8a30c5540eaf9815
Graysongo <dmitriy.chicherin@yandex.ru>
2024-12-01 20:57:31 +0300

    Подготовлен экран отображения и управления Профиелм

---
+++ ImageFeed/ProfileView/ProfileViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by dmitry.chicherin on 01.12.2024.
+//
+
+import UIKit
+
+final class ProfileViewController:UIViewController {
+    
+  
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var profileNameLabel: UILabel!
+    @IBOutlet weak var decriptionLabel: UILabel!
+    
+    @IBOutlet weak var exitButton: UIButton!
+    
+    @IBAction func didTapExitButton() {
+    }
+    
+}
+
